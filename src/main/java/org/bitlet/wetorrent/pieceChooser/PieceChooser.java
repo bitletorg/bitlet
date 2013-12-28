2c84891c024c1fb76b11de560245cbd75067da8b
Alessandro Bahgat <me@abahgat.com>
2013-12-28 14:38:54 +0100

    Updated license information
    Updated project to specify Apache License, Version 2.0

--- src/main/java/org/bitlet/wetorrent/pieceChooser/PieceChooser.java
+++ src/main/java/org/bitlet/wetorrent/pieceChooser/PieceChooser.java
@@ -1,25 +1,18 @@
 /*
  *              bitlet - Simple bittorrent library
+ *  Copyright (C) 2008 Alessandro Bahgat Shehata, Daniele Castagna
  *
- * Copyright (C) 2008 Alessandro Bahgat Shehata, Daniele Castagna
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
  *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
- *
- * Alessandro Bahgat Shehata - ale dot bahgat at gmail dot com
- * Daniele Castagna - daniele dot castagna at gmail dot com
- *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  */
 
 package org.bitlet.wetorrent.pieceChooser;
